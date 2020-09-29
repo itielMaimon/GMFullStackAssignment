6faa6694d6239321d173627af163ebc372a8faf3
Itiel Maimon <itiel.maimon@gmail.com>
2020-09-29 19:12:38 +0300

    Deployed front-end to Netlify

--- client/src/apis/ride.js
+++ client/src/apis/ride.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "https://shielded-hamlet-92028.herokuapp.com",
+  //baseURL: "http://localhost:8000",
 });
